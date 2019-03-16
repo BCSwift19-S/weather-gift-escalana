4ab3b623e947f97ee4c75902a5f549f1174f65e9
Alessandra Escalante <escalana@bc.edu>
2019-03-16 15:58:21 -0400

    7.6-7.7

--- Weather Gift/ListVC.swift
+++ Weather Gift/ListVC.swift
@@ -9,22 +9,88 @@
 import UIKit
 
 class ListVC: UIViewController {
-
+    
+    var locationsArray = [String] ()
+    var currentPage = 0
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var editBarButton: UIBarButtonItem!
+    
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self 
+     
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        if segue.identifier == "ToPageVC"{
+            let destination = segue.destination as! PageVC
+            currentPage = (tableView.indexPathForSelectedRow?.row)!
+            destination.currentPage = currentPage
+            destination.locationsArray = locationsArray
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func editBarButtonPressed(_ sender: UIBarButtonItem) {
+        if tableView.isEditing == true{ // if button clicked while editing, want the done button
+            tableView.setEditing(false, animated: true)
+            editBarButton.title = "Edit"
+            addBarButton.isEnabled = true
+        } else {
+            tableView.setEditing(true, animated: true)
+            editBarButton.title = "Done"
+            addBarButton.isEnabled = false
+        }
     }
-    */
-
 }
+extension ListVC: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locationsArray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath)
+        return cell
+    }
+    //MARK: - tableView Editing Functions
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            locationsArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let itemToMove = locationsArray[sourceIndexPath.row]
+        locationsArray.remove(at: sourceIndexPath.row)
+        locationsArray.insert(itemToMove, at: destinationIndexPath.row)
+    }
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+//        if indexPath.row != 0{
+//            return true
+//        } else {
+//            return false
+//        }
+        return(indexPath.row != 0 ? true : false)
+    }
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+//        if indexPath.row != 0{
+//            return true
+//        } else {
+//            return false
+//        }
+        return(indexPath.row != 0 ? true : false)
+    }
+    func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+//        if proposedDestinationIndexPath.row == 0 {
+//            return sourceIndexPath
+//        }else{
+//            return proposedDestinationIndexPath
+//        }
+        return(proposedDestinationIndexPath.row == 0 ? sourceIndexPath : proposedDestinationIndexPath)
+    }
+}
+
+
+
