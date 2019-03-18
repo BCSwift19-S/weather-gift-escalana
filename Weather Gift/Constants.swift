9cceddd614603938570e21910e58ffd2a62aa815
Alessandra Escalante <escalana@bc.edu>
2019-03-18 14:12:51 -0400

    7.15

---
+++ Weather Gift/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Weather Gift
+//
+//  Created by Ale Escalante on 3/18/19.
+//  Copyright © 2019 Ale Escalante. All rights reserved.
+//
+
+import Foundation
+
+let urlBase = "https://api.darksky.net/forecast/"
+let urlAPIKey = "564f9edd3d0569a08a29cc276edf95db" + "/"
